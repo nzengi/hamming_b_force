a4c09420c1018f09af55cda7ccddac8c3ab3acbe
nzengi <zenginureddin1@gmail.com>
2024-08-16 01:59:29 +0300

    Update hamming.py

--- hamming.py
+++ hamming.py
@@ -1,16 +1,15 @@
-import numpy as np
 from numba import jit, prange
+from utils import private_key_to_address
 
-# Hamming mesafesini hesaplayan fonksiyon
 @jit(nopython=True)
 def hamming_distance(a, b):
-    return bin(a ^ b).count('1')
+    return bin(int(a, 16) ^ int(b, 16)).count('1')
 
-# Belirli bir aralıkta arama yapan fonksiyon (paralel işlemli)
 @jit(nopython=True, parallel=True)
 def search_in_range(start_key, end_key, target_address, target_hamming_distance):
     for key in prange(start_key, end_key):
-        address = hash_function(key)  # hash_function ile Bitcoin adresini oluştur
+        key_hex = f"{key:064x}"  # Özel anahtarı hexadecimal formata çevir
+        address = private_key_to_address(key_hex)  # Bitcoin adresini oluştur
         hamming_dist = hamming_distance(address, target_address)
         if hamming_dist <= target_hamming_distance:
             return key  # Hedefe yakın anahtar bulundu
